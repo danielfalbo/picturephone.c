1846645df636b4bfb846a79039b0947751df8971
Daniel <39460524+danielfalbo@users.noreply.github.com>
2026-01-05 12:18:15 +0100

    update ai.sh

--- ai.sh
+++ ai.sh
@@ -7,15 +7,24 @@ trap "rm -f '$TEMP_FILE'" EXIT
 {
   echo "<picturephone.c>"
   cat "picturephone.c"
-  echo "</picturephone.c>"
+  echo "</ picturephone.c>"
 
   echo "<Makefile>"
   cat "Makefile"
-  echo "</Makefile>"
+  echo "</ Makefile>"
 
   echo "<README>"
   cat "README"
-  echo "</README>"
+  echo "</ README>"
+
+  echo "<kilo.c for reference>"
+  curl "https://raw.githubusercontent.com/antirez/kilo/master/kilo.c"
+  echo "</ kilo.c for reference>"
+
+  echo "<ertdfgcvb/play.core/src/programs/camera/camera_gray.js for reference>"
+  curl "https://raw.githubusercontent.com/ertdfgcvb/play.core/master/src/programs/camera/camera_gray.js"
+  echo "</ ertdfgcvb/play.core/src/programs/camera/camera_gray.js for reference>"
+
 } > "$TEMP_FILE"
 
 cat "$TEMP_FILE" | pbcopy
